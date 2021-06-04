dc3e929c127465b8ec627f1cf815649cc9edbe86
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-04 08:47:44 +0530

    Added: UC1 For Creating Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,16 @@
 package bridgelabz;
 
 public class TicTacToeGame {
-    public static void main(String[] args) {
+    public static char board[][] = new char[3][3];
 
+    public static void main(String[] args) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = '-';
+
+            }
+        }
     }
+
+
 }
