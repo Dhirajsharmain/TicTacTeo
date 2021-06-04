d0e8b59e2543ddd3c0af8d6767fd76ba08ca25e2
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-04 10:00:40 +0530

    Added: Choose One Letter X or O By User

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,25 @@
 package bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static char board[] = new char[10];
+    public static char userLatter;
 
     public static void main(String[] args) {
 
         createBoard();
+        chooseLetter();
+        System.out.println(userLatter);
+    }
+
+    /**
+     * @see : Method For Choose One Letter X or O By User
+     */
+    private static void chooseLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Choose One Letter X OR O That You Want TO Go With : ");
+        userLatter = scanner.next().charAt(0);
     }
 
     /**
@@ -13,9 +27,9 @@ public class TicTacToeGame {
      */
     public static void createBoard() {
         for (int i = 1; i < board.length; i++) {
-                board[i] = '-';
-            }
+            board[i] = '-';
         }
     }
-
 }
+
+
