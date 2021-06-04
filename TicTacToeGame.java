ea18286acad90cc50fbba8b55fb66785f6197747
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-04 09:19:03 +0530

    Refactor: Created Method For Board With Comment

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,8 +8,10 @@ public class TicTacToeGame {
         board();
     }
 
-    // Method For Creating Board with Empty Space
-    public static void board(){
+    /**
+     * @see : Method For Creating Board with Empty Space
+     */
+    public static void board() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 board[i][j] = '-';
